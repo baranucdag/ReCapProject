3641c7fc350290c2ce517c2f88f9a47a922b5d8c
Baran Üçdağ <75309438+baranucdag@users.noreply.github.com>
2021-06-22 17:11:11 +0300

    Add project files.

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal
+    {
+        List<Car> GetAll();
+        List<Car> GetAllByBrand(int BrandId);
+        List<Car> GetAllById(int CarId);
+        void Add(Car car);
+        void Uptade(Car car);
+        void Delete(Car car);
+   
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class NorthWindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=;Trusted_Connection=true"); //server adresi
+
+        }
+
+        //public DbSet<> MyProperty { get; set; }           //tabloları ilişkilendirme
+
+
+    }
+}
