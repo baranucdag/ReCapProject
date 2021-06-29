17c83806f53c1b3826b4bc58468e95b3cce49d55
Baran Üçdağ <75309438+baranucdag@users.noreply.github.com>
2021-06-29 15:27:36 +0300

    29.06.2021

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+
+        ICarService _carService;
+        public CarsController(ICarService productService)
+        {
+            _carService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll(Car car)
+        {
+            var result = _carService.GetAll();
+
+            return Ok(result);
+        }
+
+
+        [HttpGet("getbybrandid")]
+        public IActionResult GetByBrandId(int id)
+        {
+            var result = _carService.GetAllByBrandId(id);
+
+            return Ok(result);
+
+        }
+    }
+}
+
+    
+
