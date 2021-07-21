54104e8bd65fcf61aa56e2c2669a085636fa7206
Baran Üçdağ <75309438+baranucdag@users.noreply.github.com>
2021-07-21 19:16:55 +0300

    Autofac uptaded.

--- Business/ValidationRuıles/CarValidator.cs
+++ Business/ValidationRuıles/CarValidator.cs
@@ -12,8 +12,15 @@ namespace Business.ValidationRuıles
     {
         public CarValidator()
         {
+            RuleFor(c => c.Description).NotEmpty();
             RuleFor(c => c.DailyPrice).GreaterThan(55);
             RuleFor(c => c.ModelYear).GreaterThan(2010).When(c => c.BrandId == 1);
+            //RuleFor(c => c.Description).Must(StartWithA);
+        }
+
+        private bool StartWithA(string arg)
+        {
+            throw new NotImplementedException();
         }
     }
 }
